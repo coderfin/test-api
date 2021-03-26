9bb8c0b8092cd3d21b533d821764eefd9e6cd63a
coderfin <coderfin@gmail.com>
2021-03-26 16:49:46 -0600

    debug

---
+++ index.js
@@ -0,0 +1,20 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const registeredWorker = await navigator.serviceWorker.register(
+        "service-worker.js"
+      );
+      console.log("Worker Registered", registeredWorker);
+
+      new Array(10).forEach(() => {
+        console.log("here");
+
+        fetch("https://coderfin.github.io/test-api/", { method: "POST" })
+          .then((response) => response.text())
+          .then(console.log);
+      });
+    } catch (error) {
+      console.error("Error", error);
+    }
+  });
+}
